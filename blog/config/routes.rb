2c89f4adca9fca86fe5bf153a4b81c4487669a9d
shree786 <shyadav@bestpeers.com>
2021-03-11 17:56:03 +0530

    first commit

---
+++ blog/config/routes.rb
@@ -0,0 +1,16 @@
+# Rails.application.routes.draw do
+#   root "articles#index"
+
+#   get "/articles", to: "articles#index"
+#   get "/articles/:id", to: "articles#show"
+# end
+Rails.application.routes.draw do
+  root "articles#index"
+  resources :articles do
+    resources :comments
+  end
+  get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
+end
+
+
